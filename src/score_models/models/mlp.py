3a10a3a437d847cb79d74a845247d3e245a434f0
Robert Samoilescu <robert.samoilescu@gmail.com>
2024-04-05 20:21:47 +0100

    Included models docstrings.

--- src/score_models/models/mlp.py
+++ src/score_models/models/mlp.py
@@ -3,27 +3,29 @@ import torch.nn as nn
 
 
 class MLPBlock(nn.Module):
-    def __init__(self, input_size: int, output_size: int, t_size: int):
+    """MLP Block."""
+
+    def __init__(self, input_size: int, output_size: int, t_size: int) -> None:
+        """Constructor of the MLP Block.
+
+        :param input_size: Number of features in the input tensor
+        :param output_size: Number of features in the output tensor
+        :param t_size: Number of features in the time embedding tensor
+        """
         super().__init__()
-        self.block1 = nn.Sequential(
-            nn.BatchNorm1d(input_size),
-            nn.ReLU(),
-            nn.Linear(input_size, output_size)
-        )
+        self.block1 = nn.Sequential(nn.BatchNorm1d(input_size), nn.ReLU(), nn.Linear(input_size, output_size))
 
-        self.block2 = nn.Sequential(
-            nn.BatchNorm1d(output_size),
-            nn.ReLU(),
-            nn.Linear(output_size, output_size)
-        )
+        self.block2 = nn.Sequential(nn.BatchNorm1d(output_size), nn.ReLU(), nn.Linear(output_size, output_size))
 
-        self.temp_proj = nn.Sequential(
-            nn.BatchNorm1d(t_size),
-            nn.ReLU(),
-            nn.Linear(t_size, output_size)
-        )
+        self.temp_proj = nn.Sequential(nn.BatchNorm1d(t_size), nn.ReLU(), nn.Linear(t_size, output_size))
 
-    def forward(self, x, temb):
+    def forward(self, x: torch.Tensor, temb: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the MLP Block.
+
+        :param x: Input tensor
+        :param temb: Time embedding tensor
+        :return: Output tensor
+        """
         x = self.block1(x)
         x += self.temp_proj(temb)
         return self.block2(x)
@@ -32,43 +34,39 @@ class MLPBlock(nn.Module):
 class MLPScoreModel(nn.Module):
     """MLP Score Model."""
 
-    def __init__(self, input_size: int, hidden_size: int, L: int) -> None:
+    def __init__(self, input_size: int, hidden_size: int, T: int) -> None:
         """Constructor of the MLP Score Model.
 
         :param input_size: Number of features in the input tensor
         :param hidden_size: Number of features in the hidden layer
-        :param L: Number of batch normalization layers
+        :param T: Number of time embeddings
         """
         super().__init__()
         self.input_size = input_size
-        self.L = L
+        self.L = T
 
         self.time_embed = nn.Sequential(
             nn.Embedding(num_embeddings=self.L, embedding_dim=input_size),
             nn.Linear(input_size, hidden_size),
             nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size)
+            nn.Linear(hidden_size, hidden_size),
         )
 
         self.head = nn.Linear(input_size, hidden_size)
         self.block1 = MLPBlock(hidden_size, hidden_size, hidden_size)
         self.block2 = MLPBlock(hidden_size, hidden_size, hidden_size)
-        self.tail = nn.Sequential(
-            nn.BatchNorm1d(hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, input_size)
-        )
+        self.tail = nn.Sequential(nn.BatchNorm1d(hidden_size), nn.ReLU(), nn.Linear(hidden_size, input_size))
 
-    def forward(self, x: torch.Tensor, i: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
         """Forward pass of the MLP Score Model. Computes the
         forward pass of the model through the i-th batch
         normalization layers.
 
         :param x: Input tensor
-        :param i: Index of the batch normalization layer
+        :param t: Time tensor
         :return: Output tensor
         """
-        temb = self.time_embed(i)
+        temb = self.time_embed(t)
         x = self.head(x)
         x = self.block1(x, temb)
         x = self.block2(x, temb)
--- src/score_models/models/unet.py
+++ src/score_models/models/unet.py
@@ -1,3 +1,8 @@
+""" 
+Copyright zoubohao/DenoisingDiffusionProbabilityModel-ddpm-
+https://github.com/zoubohao/DenoisingDiffusionProbabilityModel-ddpm-/blob/main/Diffusion/Model.py
+"""
+
 import math
 import torch
 from torch import nn
@@ -6,20 +11,38 @@ from torch.nn import functional as F
 
 
 class Swish(nn.Module):
-    def forward(self, x):
+    """Swish activation function."""
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the Swish activation function.
+
+        :param x: Input tensor
+        :return: Output tensor
+        """
         return x * torch.sigmoid(x)
 
 
 class TimeEmbedding(nn.Module):
-    def __init__(self, T, d_model, dim):
+    """Time embedding module."""
+
+    def __init__(self, T: int, d_model: int, dim: int) -> None:
+        """Constructor of the TimeEmbedding module.
+
+        :param T: Number of time embeddings
+        :param d_model: Number of features in the input tensor
+        :param dim: Number of features in the output tensor
+        """
         assert d_model % 2 == 0
+
         super().__init__()
         emb = torch.arange(0, d_model, step=2) / d_model * math.log(10000)
         emb = torch.exp(-emb)
         pos = torch.arange(T).float()
         emb = pos[:, None] * emb[None, :]
+
         assert list(emb.shape) == [T, d_model // 2]
         emb = torch.stack([torch.sin(emb), torch.cos(emb)], dim=-1)
+
         assert list(emb.shape) == [T, d_model // 2, 2]
         emb = emb.view(T, d_model)
 
@@ -32,50 +55,85 @@ class TimeEmbedding(nn.Module):
         self.initialize()
 
     def initialize(self):
+        """Initialize the weights of the TimeEmbedding module."""
         for module in self.modules():
             if isinstance(module, nn.Linear):
                 init.xavier_uniform_(module.weight)
                 init.zeros_(module.bias)
 
-    def forward(self, t):
-        emb = self.timembedding(t)
-        return emb
+    def forward(self, t: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the TimeEmbedding module.
+
+        :param t: Time tensor
+        :return: Output tensor
+        """
+        return self.timembedding(t)
 
 
 class DownSample(nn.Module):
-    def __init__(self, in_ch):
+    """Downsample block."""
+
+    def __init__(self, in_ch: int) -> None:
+        """Constructor of the DownSample block.
+
+        :param in_ch: Number of input channels
+        """
         super().__init__()
         self.main = nn.Conv2d(in_ch, in_ch, 3, stride=2, padding=1)
         self.initialize()
 
     def initialize(self):
+        """Initialize the weights of the DownSample block."""
         init.xavier_uniform_(self.main.weight)
         init.zeros_(self.main.bias)
 
-    def forward(self, x, temb):
-        x = self.main(x)
-        return x
+    def forward(self, x: torch.Tensor, temb: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the DownSample block.
+
+        :param x: Input tensor
+        :param temb: Time embedding tensor
+        :return: Output tensor
+        """
+        return self.main(x)
 
 
 class UpSample(nn.Module):
-    def __init__(self, in_ch):
+    """Upsample block."""
+
+    def __init__(self, in_ch: int) -> None:
+        """Constructor of the UpSample block.
+
+        :param in_ch: Number of input channels
+        """
         super().__init__()
         self.main = nn.Conv2d(in_ch, in_ch, 3, stride=1, padding=1)
         self.initialize()
 
     def initialize(self):
+        """Initialize the weights of the UpSample block."""
         init.xavier_uniform_(self.main.weight)
         init.zeros_(self.main.bias)
 
-    def forward(self, x, temb):
+    def forward(self, x: torch.Tensor, temb: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the UpSample block.
+
+        :param x: Input tensor
+        :param temb: Time embedding tensor
+        :return: Output tensor
+        """
         _, _, H, W = x.shape
         x = F.interpolate(x, scale_factor=2, mode="nearest")
-        x = self.main(x)
-        return x
+        return self.main(x)
 
 
 class AttnBlock(nn.Module):
-    def __init__(self, in_ch):
+    """Attention block."""
+
+    def __init__(self, in_ch: int) -> None:
+        """Constructor of the Attention block.
+
+        :param in_ch: Number of input channels
+        """
         super().__init__()
         self.group_norm = nn.GroupNorm(32, in_ch)
         self.proj_q = nn.Conv2d(in_ch, in_ch, 1, stride=1, padding=0)
@@ -85,12 +143,18 @@ class AttnBlock(nn.Module):
         self.initialize()
 
     def initialize(self):
+        """Initialize the weights of the Attention block."""
         for module in [self.proj_q, self.proj_k, self.proj_v, self.proj]:
             init.xavier_uniform_(module.weight)
             init.zeros_(module.bias)
         init.xavier_uniform_(self.proj.weight, gain=1e-5)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the Attention block.
+
+        :param x: Input tensor
+        :return: Output tensor
+        """
         B, C, H, W = x.shape
         h = self.group_norm(x)
         q = self.proj_q(h)
@@ -113,7 +177,17 @@ class AttnBlock(nn.Module):
 
 
 class ResBlock(nn.Module):
-    def __init__(self, in_ch, out_ch, tdim, dropout, attn=False):
+    """Residual block."""
+
+    def __init__(self, in_ch: int, out_ch: int, tdim: int, dropout: float, attn: bool = False) -> None:
+        """Constructor of the ResBlock.
+
+        :param in_ch: Number of input channels
+        :param out_ch: Number of output channels
+        :param tdim: Number of features in the time embedding tensor
+        :param dropout: Dropout rate
+        :param attn: Whether to use attention
+        """
         super().__init__()
         self.block1 = nn.Sequential(
             nn.GroupNorm(32, in_ch),
@@ -130,35 +204,59 @@ class ResBlock(nn.Module):
             nn.Dropout(dropout),
             nn.Conv2d(out_ch, out_ch, 3, stride=1, padding=1),
         )
+
         if in_ch != out_ch:
             self.shortcut = nn.Conv2d(in_ch, out_ch, 1, stride=1, padding=0)
         else:
             self.shortcut = nn.Identity()
+
         if attn:
             self.attn = AttnBlock(out_ch)
         else:
             self.attn = nn.Identity()
+
         self.initialize()
 
     def initialize(self):
+        """Initialize the weights of the ResBlock."""
         for module in self.modules():
             if isinstance(module, (nn.Conv2d, nn.Linear)):
                 init.xavier_uniform_(module.weight)
                 init.zeros_(module.bias)
         init.xavier_uniform_(self.block2[-1].weight, gain=1e-5)
 
-    def forward(self, x, temb):
+    def forward(self, x: torch.Tensor, temb: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the ResBlock.
+
+        :param x: Input tensor
+        :param temb: Time embedding tensor
+        :return: Output tensor
+        """
         h = self.block1(x)
         h += self.temb_proj(temb)[:, :, None, None]
         h = self.block2(h)
 
         h = h + self.shortcut(x)
-        h = self.attn(h)
-        return h
+        return self.attn(h)
 
 
 class UNet(nn.Module):
-    def __init__(self, T, in_ch, ch, ch_mult, attn, num_res_blocks, dropout):
+    """UNet model."""
+
+    def __init__(
+        self, T: int, in_ch: int, ch: int, ch_mult: List[int], attn: bool, num_res_blocks: int, dropout: bool
+    ) -> None:
+        """Constructor of the UNet model.
+
+        :param T: Number of time embeddings
+        :param in_ch: Number of input channels
+        :param ch: Number of channels
+        :param ch_mult: Multiplier for the number of channels
+        :param attn: Whether to use attention
+        :param num_res_blocks: Number of residual blocks
+        :param dropout: Dropout rate
+        """
+
         super().__init__()
         assert all([i < len(ch_mult) for i in attn]), "attn index out of bound"
         tdim = ch * 4
@@ -168,14 +266,17 @@ class UNet(nn.Module):
         self.downblocks = nn.ModuleList()
         chs = [ch]  # record output channel when dowmsample for upsample
         now_ch = ch
+
         for i, mult in enumerate(ch_mult):
             out_ch = ch * mult
+
             for _ in range(num_res_blocks):
                 self.downblocks.append(
                     ResBlock(in_ch=now_ch, out_ch=out_ch, tdim=tdim, dropout=dropout, attn=(i in attn))
                 )
                 now_ch = out_ch
                 chs.append(now_ch)
+
             if i != len(ch_mult) - 1:
                 self.downblocks.append(DownSample(now_ch))
                 chs.append(now_ch)
@@ -190,51 +291,55 @@ class UNet(nn.Module):
         self.upblocks = nn.ModuleList()
         for i, mult in reversed(list(enumerate(ch_mult))):
             out_ch = ch * mult
+
             for _ in range(num_res_blocks + 1):
                 self.upblocks.append(
                     ResBlock(in_ch=chs.pop() + now_ch, out_ch=out_ch, tdim=tdim, dropout=dropout, attn=(i in attn))
                 )
                 now_ch = out_ch
+
             if i != 0:
                 self.upblocks.append(UpSample(now_ch))
-        assert len(chs) == 0
 
+        assert len(chs) == 0
         self.tail = nn.Sequential(nn.GroupNorm(32, now_ch), Swish(), nn.Conv2d(now_ch, in_ch, 3, stride=1, padding=1))
         self.initialize()
 
     def initialize(self):
+        """Initialize the weights of the UNet model."""
         init.xavier_uniform_(self.head.weight)
         init.zeros_(self.head.bias)
         init.xavier_uniform_(self.tail[-1].weight, gain=1e-5)
         init.zeros_(self.tail[-1].bias)
 
-    def forward(self, x, t):
+    def forward(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the UNet
+
+        :param x: Input tensor
+        :param t: Time tensor
+        :return: Output tensor
+        """
         # Timestep embedding
         temb = self.time_embedding(t)
+
         # Downsampling
         h = self.head(x)
         hs = [h]
+
         for layer in self.downblocks:
             h = layer(h, temb)
             hs.append(h)
+
         # Middle
         for layer in self.middleblocks:
             h = layer(h, temb)
+
         # Upsampling
         for layer in self.upblocks:
             if isinstance(layer, ResBlock):
                 h = torch.cat([h, hs.pop()], dim=1)
             h = layer(h, temb)
-        h = self.tail(h)
 
+        h = self.tail(h)
         assert len(hs) == 0
         return h
-
-
-if __name__ == "__main__":
-    batch_size = 8
-    model = UNet(T=1000, ch=128, ch_mult=[1, 2, 2, 2], attn=[1], num_res_blocks=2, dropout=0.1)
-    x = torch.randn(batch_size, 3, 32, 32)
-    t = torch.randint(1000, (batch_size,))
-    y = model(x, t)
-    print(y.shape)
